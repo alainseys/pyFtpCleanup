76d92c0d20a990b2c1bd1b87f3db84a46b6b942b
Alain Seys <alain@biopack.be>
2020-07-19 20:46:12 +0200

    toevoegen van een logging klasse

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import time
 import ftputil
+import logging
 
+logging.basicConfig(filename='ftplog.txt',level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Start program')
 ftp_host = 'enterhostname'
-ftp_username = ''
-ftp_password = ''
-ftp_path = ''
+ftp_username = 'enterusername'
+ftp_password = 'enterpassword'
+ftp_path = '/PATH/TO/DIRECOTRY/TO/PROCESS'
 
 host = ftputil.FTPHost(ftp_host,ftp_username,ftp_password)
 now = time.time()
@@ -15,5 +18,10 @@ for name in names:
         if host.path.isdir(name):
             host.rmtree(name, ignore_errors=False, onerror=None)
             print('Folder: '+ftp_path+'/'+name+ ' has been deleted from server')
-        print('ftp connection closed')
-        host.close()
+            logging.debug('Folder: '+ftp_path+'/'+name+ ' has been deleted from server')
+        else:
+            logging.debug('Noting to do')
+print('ftp connection closed')
+logging.debug('ftp connection closed')
+host.close()
+logging.debug('end of program execution')
